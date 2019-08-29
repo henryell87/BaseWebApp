b7402637dd0623f1ea28e2d240c1c811bf5f59d3
Henry Ell <henryell87@gmail.com>
2019-08-29 12:00:10 -0500

    commit all

--- project/js/main.js
+++ project/js/main.js
@@ -1,9 +1,37 @@
-function showPicture(){
-  // use jQuery ($ is shorthand) to find the div on the page and then change the html
-  // 'rounded-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
-  $("#image").append('<img class="rounded-circle" src="images/high-five.gif"/>');
-  $("p").html("High five! You're building your first web app!");
+function handelSignIn(){
+  var provider = new firebase.auth.GoogleAuthProvider();
+}
+firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a Google Access Token. You can use it to access the Google API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  var user = result.user;
+  console.log(user.email);
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+});
 
-  // jQuery can do a lot of crazy stuff, so make sure to Google around to find out more
+function addMessage(postTitle, postBody){
+var postData = {
+  title:postTitle,
+  body:postBody
+  }
+  var database = firebase.database().ref("posts");
+  var newPostRef = database.push();
+  newPostRef.set(postData)
   
+}
+
+function handelMessageFormSubmit(){
+  var postTitle = $("post-title").val();
+  var postBody = $("post-body").val();
+  addMessage(postTitle,postBody);
+  console.log(postTitle);
 }
