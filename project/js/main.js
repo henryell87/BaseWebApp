40143a80cd933dcacc5df9ca8c40ee957d0ad94c
Henry Ell <henryell87@gmail.com>
2019-09-02 10:20:00 -0500

    Updated commits

--- project/js/main.js
+++ project/js/main.js
@@ -1,3 +1,26 @@
+
+function getWeather(searchQuery) {
+  $(".error-message").text("");
+  $(".city").text("");
+  $(".temp").text("");
+  var url = "https://api.openweathermap.org/data/2.5/weather?q="+searchQuery+"&units=imperial&APPID="+apiKey;
+  $.ajax(url, {
+    success: function (data) {
+      $(".city").text(data.name);
+      $(".temp").text(data.main.temp + " °F");
+    }, error: function (error) {
+      $(".error-message").text("An error occured: "+error.responseJSON.message);
+    }
+  });
+}
+
+// This function grabs the value from the search input, then passes it along to the 
+//     getWeather() function.
+function searchWeather() {
+  var searchQuery = $('.search').val(); // grab value from search input
+  getWeather(searchQuery);
+}
+
 function handelSignIn(){
   var provider = new firebase.auth.GoogleAuthProvider();
 }
